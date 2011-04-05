7e8a9c5c63d662b0f129eb3bad34edcf8cf20b00
Michael Pope <map7777@gmail.com>
2011-04-05 16:39:29 +1000

    Add a heap of gems

--- base.rb
+++ base.rb
@@ -1,23 +1,60 @@
-# ryanb's layout tool.
-generate :nifty_layout
+# Postgresql
+gem 'pg'
+
+# JS
+gem 'jquery-rails'
 
 # View gems
-gem 'will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+gem 'haml-rails'
+gem 'sass'
+gem 'dynamic_form',:git => 'git://github.com/rails/dynamic_form.git'
+gem 'will_paginate', '~> 3.0.pre2'
 
+# Controller
+gem 'inherited_resources', :git => 'https://github.com/josevalim/inherited_resources.git'
+
+# Debugger
+gem 'ruby-debug19'
 
 # Testing gems
-gem 'rspec-rails', :group => "test"
+gem 'capybara', :group => "test"
+gem 'database_cleaner', :group => "test"
 gem 'cucumber-rails', :group => "test"
+gem 'pickle', :group => "test"
+gem 'rspec-rails', :group => "test"
+gem 'spork', :group => "test"
+gem 'launchy', :group => "test"    # So you can do Then show me the page
+gem 'webrat', :group => "test"
+
+
+gem 'machinist', '>= 2.0.0.beta1'
+gem 'faker'
+
+# PDF writer (Used for cucumber reports at the moment).
+gem "prawn"
 
 # Devise
 if yes?("Would you like to install Devise?")
   gem 'devise'
+  gem 'hpricot'
+  gem 'ruby_parser'
+  gem 'cancan'
   generate 'devise:install'
 
   model_name = ask("What would you like the user model to be called?")
   model_name = "user" if model_name.blank?
   generate 'devise', model_name
 end
+
+run 'bundle install'
+rake 'db:create', :env => 'development'
+rake 'db:create', :env => 'test'
+
+# Generate stuff
+generate 'nifty_layout --haml' # ryanb's layout tool.
+generate 'jquery:install'
+generate 'rspec:install'
+
 
 # Create new git repo.
 git :init
--- readme.txt
+++ readme.txt
@@ -1,4 +1,18 @@
+Template should include
+• all my favourite gems
+• jquery installed
+• devise configured for user.
+• my jquery plugins all configured
+• full tests done in cucumber & rspec
+• Nice stylesheet done in SCSS for basic website presentation.
 
+
+References
+http://everydayrails.com/2011/02/28/rails-3-application-templates.html
+http://blog.madebydna.com/all/code/2010/10/11/cooking-up-a-custom-rails3-template.html
+http://guides.rubyonrails.org/generators.html#application-templates
+http://railscasts.com/episodes/148-app-templates-in-rails-2-3
+http://thelastpixel.net/2010/11/21/rails-3-application-template/
 
 Use this template like so
 rails new <project name> -m https://github.com/map7/rails-templates/raw/master/base.rb
