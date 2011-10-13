4dec181675ecdbf989f3cdbbba9ecd5415887d85
Michael Pope <map7777@gmail.com>
2011-10-13 16:26:38 +1100

    Remark out nifty & configured database

--- template.rb
+++ template.rb
@@ -80,12 +80,13 @@ heading "Kaminari setup"
 generate 'kaminari:config'
 commit "Setup pagination"
 
-# Generate Nice layout using nifty-generator and convert sass to scss.
-heading "Setup layout"
-generate 'nifty:layout --haml'
-remove_file 'app/views/layouts/application.html.erb'
-generate 'nifty:config'
-commit "Setup layout"
+# Could be cause of many issues.
+# # Generate Nice layout using nifty-generator and convert sass to scss.
+# heading "Setup layout"
+# generate 'nifty:layout --haml'
+# remove_file 'app/views/layouts/application.html.erb'
+# generate 'nifty:config'
+# commit "Setup layout"
 
 # Convert application.css to sass
 # heading "Config SASS"
@@ -131,6 +132,29 @@ if @migrate_at_end
   rake 'db:test:prepare'
 end
 
+# Adding example of postgresql to database.yml
+heading "Adding postgresql example to database.yml"
+append_file 'config/database.yml' do
+  <<-eos
+
+#
+# Refer to your chef script for user & password settings
+#
+# production:
+#   adapter: postgresql
+#   encoding: unicode
+#   database: #{@app_name}_production
+#   pool: 5
+#   username: <user>
+#   password: <password>
+
+# cucumber:
+#   <<: *test
+
+eos
+end
+commit "Add postgres example to database.yml"
+
 heading "Installation complete!"
 say <<-eos
   -------------------------------------------
@@ -143,11 +167,23 @@ say <<-eos
 
   1. Configure your config/database.yml file.
 
-  2. Run the following rake tasks
+  2. Run the following rake tasks (must be done before deploy)
 
      $ rake db:create
      $ rake db:migrate
      $ rake db:test:prepare
 
   3. Enjoy!
+
+  Deploy notes:
+  -----------------------------------------------------------------------------------
+  - Check that my database.yml settings are the same as the dbuser & dbpass in chef.
+
+  - Edit the /etc/apache2/sites-enabled/rails_project on your server
+  - sudo service apache2 restart
+
+  First time deploy
+  - cap deploy:setup
+  - cap deploy:cold
+
 eos
